99d6cf36b6a521caa7850f972d4669cb90e1e6a3
vasiliy.andricov <vasiliy.andricov@WUFDR0019011.uf.rt.ru>
2017-12-20 10:36:10 +0300

    Исправлено все параметры подключения к БД берутся из командной строки

---
+++ src/main/java/ru/project/DAO/ProjectTaskFilesDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.project.DAO;
+
+import ru.project.beans.ProjectTaskFiles;
+
+/**
+ *
+ * @author vasiliy.andricov
+ */
+public class ProjectTaskFilesDAO extends abstractDAO<ProjectTaskFiles, Long> {
+
+}
---
+++ src/main/java/ru/project/DAO/ProjectTasksCommentDAO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.project.DAO;
+
+import ru.project.beans.ProjectTasksComment;
+
+/**
+ *
+ * @author vasiliy.andricov
+ */
+public class ProjectTasksCommentDAO extends abstractDAO<ProjectTasksComment, Long> {
+
+}
